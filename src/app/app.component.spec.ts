19cb7d3479b02c001a909863b9c7acd890753e96
Diego de la jara <diegodelajara@MacBook-Pro-de-Diego.local>
2024-12-13 11:30:06 -0300

    :sparkles: Add Readme and test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [MatTableModule,
+        MatInputModule,
+        MatButtonModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,NoopAnimationsModule,AppComponent],
     }).compileComponents();
   });
 
@@ -14,16 +25,42 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-task-manager' title`, () => {
+  it(`should have one input for create task name`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-task-manager');
+    expect(app.displayedColumns[0]).toEqual("name");
   });
 
-  it('should render title', () => {
+  it(`should create new row on table when clicking on add task button`, async() => {
     const fixture = TestBed.createComponent(AppComponent);
+    const nameInput = fixture.debugElement.query(By.css('input[formControlName="name"]')).nativeElement;
+    const descriptionInput = fixture.debugElement.query(By.css('input[formControlName="description"]')).nativeElement;
+    const statusInput = fixture.debugElement.query(By.css('input[formControlName="status"]')).nativeElement;
+    const addButton = fixture.debugElement.query(By.css('button[type="submit"]')).nativeElement;
+
+
+    nameInput.value = 'Tarea de prueba';
+    descriptionInput.value = 'Descripción de prueba';
+    statusInput.value = 'Pendiente';
+
+    nameInput.dispatchEvent(new Event('input'));
+    descriptionInput.dispatchEvent(new Event('input'));
+    statusInput.dispatchEvent(new Event('input'));
+
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-task-manager');
+    addButton.click();
+    fixture.detectChanges();
+
+    const rows = fixture.debugElement.queryAll(By.css('table tbody tr'));
+    expect(rows.length).toBe(1);
+    const cells = rows[0].queryAll(By.css('td'));
+    console.log(fixture.debugElement.nativeElement.innerHTML);
+
+
+    // expect(cells[0].nativeElement.textContent.trim()).toBe('Tarea de prueba');
+    // expect(cells[1].nativeElement.textContent).toBe('Descripción de prueba');
+    // expect(cells[2].nativeElement.textContent).toBe('Pendiente');
+
+
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
@@ -13,7 +12,6 @@ import { TaskService } from './services/task.service';
   imports: [
     BrowserModule,
     HttpClientModule,
-    BrowserAnimationsModule,
     MatButtonModule,
     MatSnackBarModule
   ],
